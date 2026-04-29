6245b324357ab7839d8ac5d95412d32dd004e716
Jerry <1115771425@qq.com>
2026-04-29 11:05:56 +0800

    敏感配置外部化，移除DBUtil硬编码密码

--- src/main/java/edu/haut/gradms/config/DBUtil.java
+++ src/main/java/edu/haut/gradms/config/DBUtil.java
@@ -1,25 +1,47 @@
 package edu.haut.gradms.config;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
+/**
+ * 数据库连接工具类。
+ * 连接参数从 classpath 下的 config.properties 读取，
+ * 避免将数据库密码硬编码在源码中。
+ */
 public class DBUtil {
 
-    private static final String URL =
-            "jdbc:mysql://localhost:3306/haut_graduate_management" +
-                    "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC&characterEncoding=utf8";
-    private static final String USER = "root";
-    private static final String PASSWORD = "Huiyuechen6!"; // TODO: 改成你自己的
+    private static final String URL;
+    private static final String USER;
+    private static final String PASSWORD;
 
     static {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver"); // MySQL 8 驱动
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException("加载数据库驱动失败", e);
+        try (InputStream in = DBUtil.class.getClassLoader()
+                .getResourceAsStream("config.properties")) {
+            if (in == null) {
+                throw new RuntimeException(
+                        "未找到 config.properties，请将 config.properties.example 复制为 config.properties 并填写配置");
+            }
+            Properties props = new Properties();
+            props.load(in);
+            URL      = props.getProperty("db.url");
+            USER     = props.getProperty("db.user");
+            PASSWORD = props.getProperty("db.password");
+            Class.forName(props.getProperty("db.driver", "com.mysql.cj.jdbc.Driver"));
+        } catch (IOException | ClassNotFoundException e) {
+            throw new RuntimeException("数据库配置加载失败", e);
         }
     }
 
+    /**
+     * 获取数据库连接。
+     *
+     * @return 新的 JDBC Connection，调用方负责关闭
+     * @throws SQLException 连接失败时抛出
+     */
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
